47196f42b007e21d199d0a83eb498a212e24cec0
Kris Urbas <krzysu@hidden.com>
2024-10-22 14:56:34 +0200

    add example react app to test wallet plugin

---
+++ examples/react/src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Web3Provider } from "./web3/Web3Context";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Web3Provider>
+      <App />
+    </Web3Provider>
+  </React.StrictMode>
+);
