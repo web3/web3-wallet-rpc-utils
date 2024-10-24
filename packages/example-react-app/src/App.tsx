4c11437b81859727489ef1294427803190c04ca6
Dan Forbes <dan.forbes@chainsafe.io>
2024-10-24 11:42:13 -0700

    chore(verbiage): update
    Small tweaks to update verbiage and remove boilerplate

--- packages/example-react-app/src/App.tsx
+++ packages/example-react-app/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <main>
-      <h1>Web3.js + React Minimal dApp Template</h1>
+      <h1>Web3.js Wallet RPC Methods Demonstration dApp</h1>
       <div>
         This is a sample project that demonstrates using web3-plugin-wallet-rpc.
       </div>
